60fe7d8aa86402e7cc189ea25ad5cd070eab86da
Ravi Kumar Prasad <7ravikp@gmail.com>
2023-06-30 20:10:56 +0530

    Add netlify edge deploy adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: true,
-      },
-    }),
-    preact({ compat: true }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: true
+    }
+  }), preact({
+    compat: true
+  })],
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   },
-})
+  output: "server",
+  adapter: netlify()
+});
