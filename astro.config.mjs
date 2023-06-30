78d1c8ca84ef95b5d7182bb305dec87bf4d615be
Ravi Kumar Prasad <7ravikp@gmail.com>
2023-06-30 20:21:51 +0530

    Remove SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import preact from '@astrojs/preact';
-
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    config: {
-      applyBaseStyles: true
-    }
-  }), preact({
-    compat: true
-  })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: true,
+      },
+    }),
+    preact({
+      compat: true,
+    }),
+  ],
   markdown: {
-    syntaxHighlight: 'prism'
+    syntaxHighlight: 'prism',
   },
-  output: "server",
-  adapter: netlify()
-});
+})
